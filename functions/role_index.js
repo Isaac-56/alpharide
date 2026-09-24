31c1c15e2c8ced0e1588c8584a349467d73a74fd
Yeshak Tsegaye <yeshak34@gmail.com>
2026-09-24 10:41:27 +0300

    Export driver wallet Cloud Functions

--- functions/role_index.js
+++ functions/role_index.js
@@ -3,9 +3,11 @@
 const rideFunctions = require("./index");
 const roleFunctions = require("./role_functions");
 const lifecycleFunctions = require("./lifecycle_functions");
+const walletFunctions = require("./wallet_functions");
 
 module.exports = {
   ...rideFunctions,
   ...roleFunctions,
   ...lifecycleFunctions,
+  ...walletFunctions,
 };
